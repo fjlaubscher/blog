7fb18485099f3492cd5bd209c0a3404b001811df
Francois Laubscher <francoisjlaubscher@gmail.com>
2023-04-27 21:14:23 +0200

    chore: improve home page description tag

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -13,8 +13,22 @@ import data from '../../data.json';
 
 import styles from './home.module.scss';
 
+const META_DESCRIPTION = `
+    I've played Warhammer 40,000 (on-and-off) since 4th edition, starting with a Catachan Battle Force and an old 3rd edition Codex Supplement.
+      
+    I'm much more of a hobbyist than a "competitive wargamer" (yes, it's a thing).
+    The entire process of building, painting and then playing with your models is what gets me.
+
+    Regardless of how strong / efficient some units might be, if the models suck, I'd rather
+    rather use the model that looks cool.
+    Recently, I've shifted my entire hobby focus to Bolt Action as I've gotten stuck in the world
+    of historical WWII gaming.
+    
+    I think I'm getting old.
+`;
+
 const Home = () => (
-  <Layout title="Home" description="Welcome to my blog">
+  <Layout title="Blog" description={META_DESCRIPTION}>
     <Hero />
     <About />
     <Container className={classnames(styles.container, styles.featured)}>
