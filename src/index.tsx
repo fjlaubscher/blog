a778a34aee16d7fa62fd9321c42aab62fcc5c452
Francois Laubscher <francoisjlaubscher@gmail.com>
2023-05-17 21:32:36 +0200

    chore: add vercel analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
 import Routes from './routes';
 
 import './global.scss';
 
+const isProduction = window.location.host === 'blog.francoislaubscher.dev';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes />
+      <Analytics mode={isProduction ? 'production' : 'development'} />
     </BrowserRouter>
   </React.StrictMode>
 );
